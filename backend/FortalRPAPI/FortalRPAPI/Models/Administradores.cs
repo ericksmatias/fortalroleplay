2018e365d2250344497e53b12bd4f35dcb8287b3
Érick Solon <erick.solon.prog@gmail.com>
2026-02-25 23:59:37 -0300

    Adição de algumas funcionalidades basicas

---
+++ backend/FortalRPAPI/FortalRPAPI/Models/Administradores.cs
@@ -0,0 +1,9 @@
+﻿namespace FortalRPAPI.Models
+{
+    public class Administradores
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
